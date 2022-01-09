d516ce9e4347c869796d18e3a666cf6e11523fc7
BastienVanhove <bastienv.capital@gmail.com>
2022-01-09 04:11:46 -0500

    start structure

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+  
+</template>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  --back-main-color: rgb(24, 23, 23);
+}
+body{
+  height: 100vh;
+  width: 100vw;
+}
+#app {
+  height: 100%;
+  width: 100%;
+  background-color: var(--back-main-color);
+}
+</style>
